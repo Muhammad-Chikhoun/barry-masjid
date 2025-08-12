704ac0c9ccbeb1ecac0da675e80a315896d9eef1
Muhammad-Chikhoun <mchikhoun@outlook.com>
2025-08-12 02:25:33 +0100

    create new pages, add news to home page, create live page, and base pages of other upcoming pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,47 @@
 import * as React from "react";
-import Header  from "./components/header";
+import { Routes, Route, useLocation } from "react-router-dom";
+
+import Header from "./components/header";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 
-import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import MainTable from "./components/mainTable";
 import Dev from "./components/dev";
+import Live from "./pages/live";
+import Error from "./pages/error";
 
 function App() {
+  const location = useLocation();
+  const noLayoutPages = ["/screen12345"];
+  const hideLayout = noLayoutPages.includes(location.pathname);
+
   return (
-    <>
-      <Header />
-      <Navbar />
+    <div className="flex flex-col">
+      {/* Header + Navbar */}
+      {!hideLayout && <Header />}
+      {!hideLayout && <Navbar />}
+
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/timetable" element={<MainTable />} />
-          <Route path="/live" element={<Dev />} />
+          <Route path="/live" element={<Live />} />
           <Route path="/donate" element={<Dev />} />
           <Route path="/madrassah" element={<Dev />} />
           <Route path="/contact" element={<Dev />} />
-          <Route path="*" element={<div className="p-6">404 - Not found</div>} />
+          <Route path="/screen12345" element={<Dev />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </main>
-      <Footer />
-    </>
+
+      {!hideLayout && (
+        <div className="mt-auto">
+          <Footer />
+        </div>
+      )}
+    </div>
   );
 }
+
 export default App;
