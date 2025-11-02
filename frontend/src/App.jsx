e0c76bbed275bbf7b5ac6d071d3030ad2d462d5c
Muhammad-Chikhoun <mchikhoun@outlook.com>
2025-11-02 16:15:41 +0000

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,10 @@
-import * as React from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
+import ReactDOM from "react-dom/client";
 
-import Header from "./components/header";
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
-
-import Home from "./pages/home";
-import Timetable from "./pages/timetable";
-import Contact from "./pages/contact";
-import Dev from "./components/dev";
-import Live from "./pages/live";
-import Error from "./pages/error";
-import Screen from "./pages/screen";
-import Donation from "./pages/donation";
-
-function App() {
-  const location = useLocation();
-  const noLayoutPages = ["/screen12345"];
-  const hideLayout = noLayoutPages.includes(location.pathname);
-
-  return (
-    <div className="flex flex-col">
-      {/* Header + Navbar */}
-      {!hideLayout && <Header />}
-      {!hideLayout && <Navbar />}
-
-      <main>
-        <Routes>
-          <Route path="barry-masjid/" element={<Home />} />
-          <Route path="barry-masjid/timetable" element={<Timetable />} />
-          <Route path="barry-masjid/live" element={<Live />} />
-          <Route path="barry-masjid/donate" element={<Donation />} />
-          <Route path="barry-masjid/madrassah" element={<Dev />} />
-          <Route path="barry-masjid/contact" element={<Contact />} />
-          <Route path="barry-masjid/screen12345" element={<Screen />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </main>
-
-      {!hideLayout && (
-        <div className="mt-auto">
-          <Footer />
-        </div>
-      )}
-    </div>
-  );
-}
-
-export default App;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <HashRouter basename="/barry-masjid">
+    <App />
+  </HashRouter>
+);
